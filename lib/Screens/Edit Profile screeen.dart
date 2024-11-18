a3c3195c6d5cbe28291f9e7be8604901165950d2
MYPC <Charangattu643@gmail.com>
2024-11-18 14:52:52 +0530

    Committed by CHARAN 18-11-2024 AT 2:52 PM

--- lib/Screens/Edit Profile screeen.dart
+++ lib/Screens/Edit Profile screeen.dart
@@ -10,6 +10,7 @@ import '../Services/UserApi.dart';
 import '../providers/UserDetailsProvider.dart';
 import '../utils/CustomAppBar1.dart';
 import '../utils/CustomSnackBar.dart';
+import '../utils/ShakeWidget.dart';
 
 class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({Key? key}) : super(key: key);
@@ -29,6 +30,8 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   bool isLoading = true; // Loading state
   XFile? _pickedFile;
   // CroppedFile? _croppedFile;
+  String _validatePhone = "";
+  String _validateEmail = "";
 
   final ImagePicker _picker = ImagePicker();
 
@@ -36,6 +39,21 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   void initState() {
     super.initState();
     _fetchUserProfile(); // Fetch user profile data when the screen is initialized
+  }
+
+
+  void _validateFields() {
+    setState(() {
+      _validatePhone =
+      mobileController.text.length < 10 ? "Please enter a valid phone number (10 digits)" : "";
+      _validateEmail = !RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").hasMatch(emailController.text)
+          ? "Please enter a valid email address (e.g. user@domain.com)"
+          : "";
+    });
+
+    if (_validatePhone.isEmpty && _validateEmail.isEmpty) {
+      _updateProfile();
+    }
   }
 
   String profile_image = "";
@@ -147,210 +165,80 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   ],
                 ),
               ),
+
               SizedBox(height: 16),
-              Label(text: ' Full Name'),
+              Label(text: 'Full Name'),
               SizedBox(height: 4),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.05,
-                child: TextFormField(
-                  controller: fullnameController,
-                  focusNode: _focusNodeFullName,
-                  keyboardType: TextInputType.text,
-                  cursorColor: Color(0xffCAA16C),
-                  decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                    hintText: "Enter Name",
-                    hintStyle: const TextStyle(
-                      fontSize: 14,
-                      letterSpacing: 0,
-                      height: 25.73 / 14,
-                      color: Color(0xffCAA16C),
-                      fontFamily: 'RozhaOne',
-                      fontWeight: FontWeight.w400,
-                    ),
-                    prefixIcon: Container(
-                      width: 24,
-                      height: 24,
-                      padding: EdgeInsets.only(top: 10, bottom: 10, left: 6),
-                      child: Image.asset(
-                        "assets/person.png",
-                        fit: BoxFit.contain,
-                        color: Color(0xffCAA16C),
-                      ),
-                    ),
-                    filled: true,
-                    fillColor: const Color(0xffFCFAFF),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                  ),
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: 'RozhaOne',
-                    overflow:
-                        TextOverflow.ellipsis, // Add ellipsis for long text
-                  ),
-                  textAlignVertical: TextAlignVertical.center,
-                ),
+              _buildTextField(
+                controller: fullnameController,
+                hint: "Enter Name",
+                icon: "assets/person.png",
+                focusNode: _focusNodeFullName,
               ),
               SizedBox(height: 16),
               Label(text: 'Mobile Number'),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.050,
-                child: TextFormField(
-                  controller: mobileController,
-                  focusNode: _focusNodePhone,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                    LengthLimitingTextInputFormatter(10)
-                  ],
-                  cursorColor: Color(0xffCAA16C),
-                  decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                    hintText: "Enter Mobile Number",
-                    hintStyle: const TextStyle(
-                      fontSize: 14,
-                      letterSpacing: 0,
-                      height: 25.73 / 14,
-                      color: Color(0xffCAA16C),
-                      fontFamily: 'RozhaOne',
-                      fontWeight: FontWeight.w400,
-                    ),
-                    prefixIcon: Container(
-                      width: 24,
-                      height: 24,
-                      padding: EdgeInsets.only(top: 10, bottom: 10, left: 6),
-                      child: Image.asset(
-                        "assets/call.png",
-                        fit: BoxFit.contain,
-                        color: Color(0xffCAA16C),
+              _buildTextField(
+                controller: mobileController,
+                hint: "Enter Mobile Number",
+                icon: "assets/call.png",
+                focusNode: _focusNodePhone,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(10),
+                ],
+              ),
+              if (_validatePhone.isNotEmpty)
+                Container(
+                  alignment: Alignment.topLeft,
+                  margin: EdgeInsets.only(left: 8, bottom: 10, top: 5),
+                  width: w * 0.6,
+                  child: ShakeWidget(
+                    key: Key("value"),
+                    duration: Duration(milliseconds: 700),
+                    child: Text(
+                      _validatePhone,
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 12,
+                        color: Colors.red,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                    filled: true,
-                    fillColor: const Color(0xffFCFAFF),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
                   ),
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: 'RozhaOne',
-                    overflow:
-                        TextOverflow.ellipsis, // Add ellipsis for long text
-                  ),
-                  textAlignVertical: TextAlignVertical.center,
                 ),
-              ),
               SizedBox(height: 16),
               Label(text: 'Email'),
-              SizedBox(
-                height: 4,
+              _buildTextField(
+                controller: emailController,
+                hint: "Enter Email Address",
+                icon: "assets/mail.png",
+                focusNode: _focusNodeEmail,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r"[a-zA-Z0-9@._-]")),
+                ],
               ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.050,
-                child: TextFormField(
-                  controller: emailController,
-                  focusNode: _focusNodeEmail,
-                  keyboardType: TextInputType.emailAddress,
-                  cursorColor: Color(0xffCAA16C),
-                  maxLines: 1,
-
-                  decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                    hintText: "Enter Email Address",
-                    hintStyle: const TextStyle(
-                      fontSize: 14,
-                      letterSpacing: 0,
-                      height: 25.73 / 14,
-                      color: Color(0xffCAA16C),
-                      fontFamily: 'RozhaOne',
-                      fontWeight: FontWeight.w400,
-                    ),
-                    prefixIcon: Container(
-                      width: 24,
-                      height: 24,
-                      padding: EdgeInsets.only(top: 10, bottom: 10, left: 6),
-                      child: Image.asset(
-                        "assets/mail.png",
-                        fit: BoxFit.contain,
-                        color: Color(0xffCAA16C),
+              if (_validateEmail.isNotEmpty)
+                Container(
+                  alignment: Alignment.topLeft,
+                  margin: EdgeInsets.only(left: 8, bottom: 10, top: 5),
+                  width: w * 0.6,
+                  child: ShakeWidget(
+                    key: Key("value"),
+                    duration: Duration(milliseconds: 700),
+                    child: Text(
+                      _validateEmail,
+                      style: TextStyle(
+                        fontFamily: "Poppins",
+                        fontSize: 12,
+                        color: Colors.red,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                    filled: true,
-                    fillColor: const Color(0xffFCFAFF),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      borderSide:
-                          const BorderSide(width: 1, color: Color(0xffCAA16C)),
-                    ),
                   ),
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: 'RozhaOne',
-                    overflow:
-                        TextOverflow.ellipsis, // Add ellipsis for long text
-                  ),
-                  textAlignVertical:
-                      TextAlignVertical.center, // Vertically center the text
                 ),
-              ),
               SizedBox(height: 40),
               InkResponse(
-                onTap: () {
-                  _updateProfile();
-                },
+                onTap: _validateFields,
                 child: Container(
                   width: w,
                   height: MediaQuery.of(context).size.height * 0.060,
@@ -360,18 +248,17 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   ),
                   child: Center(
                     child: isLoading
-                        ? spinkits.getFadingCircleSpinner(
-                            color: Color(0xffE7C6A0))
+                        ? CircularProgressIndicator(color: Color(0xffE7C6A0))
                         : Text(
-                            "SAVE",
-                            style: TextStyle(
-                              color: Color(0xffCAA16C),
-                              fontFamily: "RozhaOne",
-                              fontWeight: FontWeight.w400,
-                              fontSize: 16,
-                              height: 21 / 16,
-                            ),
-                          ),
+                      "SAVE",
+                      style: TextStyle(
+                        color: Color(0xffCAA16C),
+                        fontFamily: "RozhaOne",
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                        height: 21 / 16,
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -382,14 +269,70 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     );
   }
 
-  Widget Label({required String text, TextAlign? textalign}) {
-    return Text(text,
-        textAlign: textalign,
-        style: TextStyle(
-            color: Color(0xff110B0F),
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String hint,
+    required String icon,
+    required FocusNode focusNode,
+    List<TextInputFormatter>? inputFormatters,
+  }) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.05,
+      child: TextFormField(
+        controller: controller,
+        focusNode: focusNode,
+        inputFormatters: inputFormatters,
+        cursorColor: Color(0xffCAA16C),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+          hintText: hint,
+          hintStyle: const TextStyle(
+            fontSize: 14,
+            color: Color(0xffCAA16C),
             fontFamily: 'RozhaOne',
-            fontSize: 15,
-            height: 21.3 / 15,
-            fontWeight: FontWeight.w400));
+            fontWeight: FontWeight.w400,
+          ),
+          prefixIcon: Container(
+            width: 24,
+            height: 24,
+            padding: EdgeInsets.only(top: 10, bottom: 10, left: 6),
+            child: Image.asset(
+              icon,
+              fit: BoxFit.contain,
+              color: Color(0xffCAA16C),
+            ),
+          ),
+          filled: true,
+          fillColor: const Color(0xffFCFAFF),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7),
+            borderSide: const BorderSide(width: 1, color: Color(0xffCAA16C)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7),
+            borderSide: const BorderSide(width: 1, color: Color(0xffCAA16C)),
+          ),
+        ),
+        style: TextStyle(
+          fontSize: 14,
+          fontFamily: 'RozhaOne',
+          overflow: TextOverflow.ellipsis,
+        ),
+        textAlignVertical: TextAlignVertical.center,
+      ),
+    );
+  }
+
+  Widget Label({required String text, TextAlign? textalign}) {
+    return Text(
+      text,
+      textAlign: textalign,
+      style: TextStyle(
+        color: Color(0xff110B0F),
+        fontFamily: 'RozhaOne',
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+      ),
+    );
   }
 }
